0441f750c7f59e8d5760dff06251bb26b0f8c613
Jesus Flores <jesus.flores.angulo@gmail.com>
2023-07-27 11:35:49 -0500

    Ventana de seleccion lista, agregandole unas ultimas cosas

--- index.py
+++ index.py
@@ -5,11 +5,22 @@ class Product:
 
     def __init__(self, window):
         
+        #+++++++++++++++++++
+        # Windows Attributes
+        #+++++++++++++++++++
+
         self.wind = window
         self.wind.title('Contacts')
-        self.wind.geometry('800x800')
+        self.wind.geometry('800x600')
         self.wind.resizable(False, False)
         self.wind.configure(bg='gray')
+
+        self.slctxt = Canvas(self.wind, bg='SpringGreen2', width=700, height=230)
+        self.slctxt.create_text(300, 50, text='Select one method to save the contacts', fill="black", font=('Arial 20 bold'))
+        self.slctxt.pack(side=TOP)
+        self.localctc = Button(self.wind, text='Save Contacts Locally', width=25, height=2, bg='gray', fg='white', font=('Arial 20 bold'))
+        self.localctc.place(relx=0.5, rely=0.5, anchor=CENTER)
+
 
 
 if __name__ == '__main__':
