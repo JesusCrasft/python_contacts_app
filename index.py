67da583ccd1f49b8bfe1dd76d2bcd60f29e9154b
Jesus Flores <jesus.flores.angulo@gmail.com>
2023-07-28 10:51:50 -0500

    Se cambio el codigo  de la ventana secundaria, se cambiaron los nombres de las variables para crear las ventanas y todo se realizo correctamente

--- index.py
+++ index.py
@@ -3,42 +3,47 @@ from tkinter import *
 
 class Product:
 
-    def __init__(self, window):
+    def __init__(self, PryWind):
     
-        # Windows Attributes
+        # Primary Window Attributes
 
-        self.PryWind = window
-        self.PryWind.title('Contacts')
-        self.PryWind.geometry('800x600')
-        self.PryWind.resizable(False, False)
-        self.PryWind.configure(bg='#1F1F1F')
+        self.windOne = PryWind
+        self.windOne.title('Contacts')
+        self.windOne.geometry('800x600')
+        self.windOne.resizable(False, False)
+        self.windOne.configure(bg='#1F1F1F')
 
         #Primary Window
 
         #Canvas
-        self.slctxt = Canvas(self.PryWind, width=780, height=75, bg='#1F1F1F', relief='groove',highlightthickness='0')
+        self.slctxt = Canvas(self.windOne, width=780, height=75, bg='#1F1F1F', relief='groove',highlightthickness='0')
         self.slctxt.create_text(390, 25, text='Select one method to save the contacts', fill='white', font=('Arial 15 bold'))
         self.slctxt.place(relx=0.5, rely=0.1, anchor=CENTER)
 
         #Buttons
-        self.localctc = Button(self.PryWind, text='Save Contacts Locally', command=self.SryWfunc)
+        self.localctc = Button(self.windOne, text='Save Contacts Locally', command=self.SryWfunc)
         self.localctc.configure(width=25, height=2, bg='#1F1F1F', fg='white', highlightthickness='0', font=('Arial 15 bold'))
         self.localctc.place(relx=0.5, rely=0.5, anchor=CENTER)
 
+
         #Secondary Window
 
+    #Function: Secondary window creation
     def SryWfunc(self):
-        self.PryWind.destroy()
+        self.windOne.destroy()
 
-        self.SryWind = Toplevel()
-        self.SryWind.title('Local Contacts')
-        self.SryWind.geometry('800x600')
-        self.SryWind.resizable(False, False)
-        self.SryWind.configure(bg='#1F1F1F')
+        # Secondary Window Attributes
+        
+        SryWind = Tk()
+        self.windTwo = SryWind
+        self.windTwo.title('Local Contacts')
+        self.windTwo.geometry('800x600')
+        self.windTwo.resizable(False, False)
+        self.windTwo.configure(bg='#1F1F1F')
 
 
 
 if __name__ == '__main__':
-    window = Tk()
-    application = Product(window)
-    window.mainloop()
+    PryWind = Tk()
+    application = Product(PryWind)
+    PryWind.mainloop()
