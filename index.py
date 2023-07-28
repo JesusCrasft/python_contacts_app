22b49ea5a5bdf75bb198b26878f3d96e51fceb31
Jesus Flores <jesus.flores.angulo@gmail.com>
2023-07-28 10:32:34 -0500

    Se agrego la ventana secundaria y se cambiaron los nombres de las variables de vetanas

--- index.py
+++ index.py
@@ -7,25 +7,34 @@ class Product:
     
         # Windows Attributes
 
-        self.wind = window
-        self.wind.title('Contacts')
-        self.wind.geometry('800x600')
-        self.wind.resizable(False, False)
-        self.wind.configure(bg='#1F1F1F')
+        self.PryWind = window
+        self.PryWind.title('Contacts')
+        self.PryWind.geometry('800x600')
+        self.PryWind.resizable(False, False)
+        self.PryWind.configure(bg='#1F1F1F')
 
         #Primary Window
 
         #Canvas
-        self.slctxt = Canvas(self.wind, width=780, height=75, bg='#1F1F1F', relief='groove',highlightthickness='0')
+        self.slctxt = Canvas(self.PryWind, width=780, height=75, bg='#1F1F1F', relief='groove',highlightthickness='0')
         self.slctxt.create_text(390, 25, text='Select one method to save the contacts', fill='white', font=('Arial 15 bold'))
         self.slctxt.place(relx=0.5, rely=0.1, anchor=CENTER)
 
         #Buttons
-        self.localctc = Button(self.wind, text='Save Contacts Locally')
+        self.localctc = Button(self.PryWind, text='Save Contacts Locally', command=self.SryWfunc)
         self.localctc.configure(width=25, height=2, bg='#1F1F1F', fg='white', highlightthickness='0', font=('Arial 15 bold'))
         self.localctc.place(relx=0.5, rely=0.5, anchor=CENTER)
 
         #Secondary Window
+
+    def SryWfunc(self):
+        self.PryWind.destroy()
+
+        self.SryWind = Toplevel()
+        self.SryWind.title('Local Contacts')
+        self.SryWind.geometry('800x600')
+        self.SryWind.resizable(False, False)
+        self.SryWind.configure(bg='#1F1F1F')
 
 
 
