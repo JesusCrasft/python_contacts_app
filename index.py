5e9e9059bb546398511fd30d9804fad9e337c376
Jesus Flores <jesus.flores.angulo@gmail.com>
2023-08-01 11:45:08 -0500

    Se cambiaron los nombres de las variables a nombres mas apropiados

--- index.py
+++ index.py
@@ -16,14 +16,14 @@ class Product:
         #Primary Window
 
         #Canvas
-        self.seltxt = Canvas(self.windOne, width=780, height=75, bg='#1F1F1F', relief='groove',highlightthickness='0')
-        self.seltxt.create_text(390, 25, text='Select one method to save the contacts', fill='white', font=('Arial 15 bold'))
-        self.seltxt.place(relx=0.5, rely=0.1, anchor=CENTER)
+        self.SelTxt = Canvas(self.windOne, width=780, height=75, bg='#1F1F1F', relief='groove',highlightthickness='0')
+        self.SelTxt.create_text(390, 25, text='Select one method to save the contacts', fill='white', font=('Arial 15 bold'))
+        self.SelTxt.place(relx=0.5, rely=0.1, anchor=CENTER)
 
         #Buttons
-        self.localctc = Button(self.windOne, text='Save Contacts Locally', command=self.SryWfunc)
-        self.localctc.configure(width=25, height=2, bg='#1F1F1F', fg='white', highlightthickness='0', font=('Arial 15 bold'))
-        self.localctc.place(relx=0.5, rely=0.5, anchor=CENTER)
+        self.LocalCtc = Button(self.windOne, text='Save Contacts Locally', command=self.SryWfunc)
+        self.LocalCtc.configure(width=25, height=2, bg='#1F1F1F', fg='white', highlightthickness='0', font=('Arial 15 bold'))
+        self.LocalCtc.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 
         #Secondary Window
@@ -42,13 +42,13 @@ class Product:
         self.windTwo.configure(bg='#1F1F1F')
 
         #Labels
-        self.lblbtn = Label(self.windTwo, height=2, width=29)
-        self.lblbtn.configure(background='#1F1F1F', relief=SOLID, borderwidth=2)
-        self.lblbtn.place(x=149, y=52, anchor=S)
+        self.LblBtn = Label(self.windTwo, height=2, width=29)
+        self.LblBtn.configure(background='#1F1F1F', relief=SOLID, borderwidth=2)
+        self.LblBtn.place(x=149, y=52, anchor=S)
 
-        self.lblsrc = Label(self.windTwo, height=23, width=29)
-        self.lblsrc.configure(background='#1F1F1F', relief=SOLID, borderwidth=2)
-        self.lblsrc.place(x=149, y=52, anchor=N)
+        self.LblSrc = Label(self.windTwo, height=23, width=29)
+        self.LblSrc.configure(background='#1F1F1F', relief=SOLID, borderwidth=2)
+        self.LblSrc.place(x=149, y=52, anchor=N)
 
 
         #Buttons
@@ -56,22 +56,23 @@ class Product:
         # Buttons Navigation
 
         #Add Contacts
-        self.addctc = Button(self.lblbtn, text='+')
-        self.addctc.config(height=1, width=1, font='Arial 5')
-        self.addctc.place(x=50, y=22, anchor=E)
+        self.AddCtc = Button(self.lblbtn, text='+')
+        self.AddCtc.config(height=1, width=1, font='Arial 5')
+        self.AddCtc.place(x=50, y=22, anchor=E)
 
         #Select Contacts
-        self.selctc = Button(self.lblbtn, text='+')
-        self.selctc.config(height=1, width=1, font='Arial 5')
-        self.selctc.place(x=260, y=22, anchor=E)
+        self.SelCtc = Button(self.lblbtn, text='+')
+        self.SelCtc.config(height=1, width=1, font='Arial 5')
+        self.SelCtc.place(x=260, y=22, anchor=E)
 
         #Settings
-        self.sngs = Button(self.lblbtn, text='+')
-        self.sngs.config(height=1, width=1, font='Arial 5')
-        self.sngs.place(x=210, y=22, anchor=E)
+        self.Sngs = Button(self.lblbtn, text='+')
+        self.Sngs.config(height=1, width=1, font='Arial 5')
+        self.Sngs.place(x=210, y=22, anchor=E)
+
 
         #Search Bar
-
+        self.SrcCtc
 
 
 
